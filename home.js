287eb8ea5dbff8b78a59f5278897d7152f26863b
vaishali <guptavaishali628@gmail.com>
2025-10-12 22:23:30 +0530

    Day 3

---
+++ home.js
@@ -0,0 +1,10 @@
+// functions to redirect on signup and login page through buttons
+let signUp=()=>{
+    location.href="sign-up.html"
+    return false;
+}
+
+let login=()=>{
+    location.href="sign-in.html";
+    return false;
+}
