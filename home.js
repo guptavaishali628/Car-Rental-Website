5ae90a46f768cf8f596d93eebf20e55a7729a603
vaishali <guptavaishali628@gmail.com>
2025-10-31 00:29:05 +0530

    Day 5

--- home.js
+++ home.js
@@ -1,10 +1,27 @@
 // functions to redirect on signup and login page through buttons
 let signUp=()=>{
-    location.href="sign-up.html"
+    location.href="sign-up.html";
     return false;
 }
 
 let login=()=>{
     location.href="sign-in.html";
+    return false;
+}
+
+let logout=()=>{
+let Confirm_logout=confirm("Do you really want to logout?")
+if(Confirm_logout){
+    location.href = "sign-up.html";
+    return false;
+}
+else{
+    location.href = "home.html";
+    return false;
+}
+}
+
+let rentNow=()=>{
+    location.href = "booking.html";
     return false;
 }
