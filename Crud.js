f822a89c52fa59cd21ad20580dc0cdc97a9334df
vaishali <guptavaishali628@gmail.com>
2025-11-02 23:24:15 +0530

    Day 6

--- Crud.js
+++ Crud.js
@@ -24,7 +24,8 @@ let fetchData = async()=>{
             <td>${e.Bvehicle}</td>
             <td>${e.Bpeople}</td>
             <td>${e.Price}</td>
-            <td onclick="del('${e.id}')">Cancel</td>
+            <td onclick="del('${e.id}')"><i class="fa fa-trash" aria-hidden="true"></i></td>
+            <td onclick="formopen('${e.id}')><i class="fa-solid fa-pen"></i></td>
         </tr>
         
         `
@@ -33,8 +34,14 @@ let fetchData = async()=>{
 
 //DELETE METHOD:
 let del = (id) =>{
+    //------------------eror handling (try catch)------------------//
+    try{
     let api = `http://localhost:3000/CarRental/${id}`  // id se delete krenge isliye id uthayenge 
     fetch(api,{method: "DELETE"})
+    }
+    catch(err){
+     console.log("Server Error", err)
+    }
 }    
 
 //POST METHOD:
@@ -79,3 +86,75 @@ let book=()=>{
 }
 
 //fetchData() ----> already call on body by "onload()":
+
+let formopen=async(id)=>{
+    let show = document.querySelector("#formshow");
+    
+    //GET method
+    let api=`http://localhost:3000/CarRental/${id}`
+    
+    let res=await fetch(api,{method:"GET"})
+    let data=await res.json();
+    console.log(data);
+
+    show.innerHTML=`
+    <div class="booking-form-text">
+           <center><form data-aos="fade-down" data-aos-duration="2000">
+                
+                <label>Full Name</label><br>
+                <input type="text" id="Bname" placeholder="Enter you Name" value="${data.Name}"><br>
+
+                <label>Contact Number</label><br>
+                <input type="text" id="Bnumber" placeholder="Enter your Contact Number" value="${data.Number}"><br>
+
+                <label>Driver's License Number</label><br>
+                <input type="text" id="Blicense" placeholder="Enter you License Number" value="${data.License}"><br>
+
+                <label>City</label>
+                <select id="Bcity>
+                    <option value="Select">Select your City</option>
+                    <option value="Bhopal">Bhopal</option>
+                    <option value="Indore">Indore</option>
+                    <option value="Ujjain">Ujjain</option>
+                    <option value="Gwalior">Gwalior</option>
+                    <option value="Jhanshi">Jhanshi</option>
+                    <option value="Lucknow">Lucknow</option>
+                    <option value="Pune">Pune</option>
+                    <option value="Mumbai">Mumbai</option>
+                    <option value="Delhi">Delhi</option>
+                </select><br>
+
+                <label>Enter your Pickup Location</label><br>
+                <input type="text" id="Bpickup" placeholder="Enter your Pickup-Location" value="${data.Pickup}"><br>
+
+                <label>Enter your Drop-off Location</label><br>
+                <input type="text" id="BdropOff" placeholder="Enter your Pickup-Location" value="${data.Dropoff}"><br>
+
+                <label>Select your Pickup Date</label><br>
+                <input type="date" id="BpickupDate" value="${data.PickupDate}"><br>
+
+                <label>Select your Return Date</label><br>
+                <input type="date" id="BreturnDate" value="${data.ReturnDate}"><br>
+
+                <label>Vehicle</label><br>
+                <select id="Bvehicle">
+                    <option value="Select">Select your fav Vehicle</option>
+                    <option value="BMW">BMW</option>
+                    <option value="Tata">Tata</option>
+                    <option value="Mercedes">Mercedes</option>
+                    <option value="Harrier">Harrier</option>
+                    <option value="Thar">Thar</option>
+                    <option value="Defender">Defender</option>
+                    <option value="Kia">Kia</option>
+                    <option value="XUV-300">XUV-300</option>
+                </select><br>
+
+                <label>Number of People</label><br>
+                <input type="text" id="Bpeople" placeholder="Enter number of people" value="${data.People}"><br>
+
+                <input type="submit" value="Update Now" onclick="return update('${data.id}')" style="cursor: pointer; width: 20%;" class="btn"></input>
+            
+            </form></center>
+        </div>
+    `
+}
